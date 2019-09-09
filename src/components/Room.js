d29e846c8a8232cc04500aa9a25ae40cfb21efc8
Emily Fox <EmilyFoxRoberts@Emilys-MacBook.local>
2019-09-09 18:28:30 +0100

    bordered edge tiles

--- src/components/Room.js
+++ src/components/Room.js
@@ -2,15 +2,14 @@ import React, { useState } from 'react'
 import styled, { css } from 'styled-components'
 import { createRoom } from '../../utils/createRoom'
 
-const TILE_SIZE = 16
-const BORDER_WIDTH = 1
+const TILE_SIZE = 54
+const BORDER_WIDTH = 2
 
 const Container = styled.div`
   border: 1px solid #333;
   display: flex;
-  flex: 1;
   flex-direction: column;
-  padding: ${TILE_SIZE}px;
+  padding: ${TILE_SIZE * 1.5}px;
 `
 
 const Row = styled.div`
@@ -26,11 +25,24 @@ const tileStyles = props => {
   }
 }
 
+const tileBorder = props => {
+  const { n, e, s, w } = props.options
+  return css`
+    border-top-color: rgba(100,0,0,${n ? 0.5 : 0.2});
+    border-right-color: rgba(100,0,0,${e ? 0.5 : 0.2});
+    border-bottom-color: rgba(100,0,0,${s ? 0.5 : 0.2});
+    border-left-color: rgba(100,0,0,${w ? 0.5 : 0.2});
+  `
+}
+
 const Tile = styled.div`
   padding: ${(TILE_SIZE - BORDER_WIDTH * 2) / 2}px;
   background: ${props => props.theme.tileColors[tileStyles(props)]};
-  border: ${BORDER_WIDTH}px solid rgba(0,0,0,0.3);
+  border-width: ${BORDER_WIDTH}px;
+  border-style: solid;
+  ${tileBorder};
 `
+// border: ${BORDER_WIDTH}px solid rgba(0,0,0,0.3);
 
 class Room extends React.Component {
   state = { room: [] }
@@ -52,10 +64,16 @@ class Room extends React.Component {
         <Container>
           {room.map((row, rowIndex) => (
             <Row length={row.length} key={rowIndex}>
-              {row.map((tile, colIndex) =>
+              {row.map(({ type, ...options }, colIndex) =>
                 <Tile
                   key={`${rowIndex}-${colIndex}`}
-                  value={tile}
+                  options={{
+                    n: rowIndex === 0,
+                    e: colIndex === row.length - 1,
+                    s: rowIndex === room.length - 1,
+                    w: colIndex === 0,
+                  }}
+                  value={type}
                 />
               )}
             </Row>
--- utils/create2dArray.js
+++ utils/create2dArray.js
@@ -1,9 +1,21 @@
-export const create2dArray = (rows, columns, num = 1) => {
+const TILE = {
+  n: false,
+  ne: false,
+  e: false,
+  se: false,
+  s: false,
+  sw: false,
+  w: false,
+  nw: false,
+  type: 0,
+}
+
+export const create2dArray = (rows, columns, value = TILE) => {
   let array = []
   for (var i = 0; i < rows; i++) {
     array.push([])
     for (var j = 0; j < columns; j++) {
-      array[i].push(num)
+      array[i].push(TILE)
     }
   }
   return array
